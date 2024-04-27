bugfix length calculation

ga.py
@@ -36,6 +36,8 @@ class Route:
         if not self.paths:
             self.initialize_paths()
         return random.randint(0, len(self.paths)-1)
+    
+    
 
 class Solution:
     # A route is a jeep    
@@ -60,14 +62,14 @@ class Solution:
         routes = self.get_path_choices()
         overall_weight = 0
         for route in routes:
-            overall_weight += route[1]['total_weight']
+            overall_weight += utils.get_path_distance(route[1]['path'])
         return overall_weight
 
     def get_overall_weight_ave(self):
         routes = self.get_path_choices()
         path_lengths = []
         for route in routes:
-            path_lengths.append(route[1]['total_weight'])
+            path_lengths.append(utils.get_path_distance(route[1]['path']))
         
         return np.mean(path_lengths)
 
@@ -296,11 +298,14 @@ if __name__ == "__main__":
         for path in best_path_choices:
             route = path[0]
             path_taken = path[1]['path']
+            path_distance = utils.get_path_distance(path_taken)
+            num_nodes = utils.get_num_nodes(path_taken)
             f.write("-"*50)
-            f.write(f"\nRoute {route} | Nodes Visited {path[1]['num_visited']} | Distance {path[1]['total_weight']}\n")
+            f.write(f"\nRoute {route} | Nodes Visited {num_nodes} | Distance {path_distance}\n")
             f.write("->".join(path_taken))
             f.write("\n")
             f.write("-"*50)
+            
 
 
     print("results saved to results.txt")

utils.py
@@ -132,8 +132,6 @@ def get_acceptable_congestion_rate(counts):
             total += 1
             if count/30 <= get_acceptable_congestion(outer, inner):
                 acceptable +=1
-            # else:
-            #     print(f"Congestion at ({outer},{inner}) : {count/30} ")
     
     return [acceptable/total, total, acceptable]
 
@@ -157,4 +155,26 @@ def get_average_congestion_diff(prev, curr):
             else:
                 improvements.append(0)
 
-    return np.mean(improvements)            
+    return np.mean(improvements)
+
+def get_num_nodes(path):
+    return len(path)
+
+def get_edge_distance(src, dest, path):
+    path_taken = "->".join(path)
+    distance = 0
+    for neighbor in landmarks[src]:
+        if neighbor[0] == dest:
+            distance = neighbor[1]
+    if distance > 0:
+        return distance
+    print(f"Missing connection: {src} -> {dest}")
+    print(f"Path Taken {path_taken}")
+    raise Exception("Missing Link")
+
+def get_path_distance(path):
+    total_distance = 0
+    for i in range(len(path) - 1):
+        total_distance += get_edge_distance(path[i], path[i+1], path)
+
+    return total_distance